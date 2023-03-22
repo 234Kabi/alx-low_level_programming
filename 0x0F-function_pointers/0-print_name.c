eb7eb3111a1ee28bfcb77203a0af0bfc99e94798
234Kabi <234kabi@gmail.com>
2023-03-22 13:59:15 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
 /**
- * print_name_as_is - a function that prints a name.
- * @name: the name Kabi
- * @f: function of name
+ * print_name - function to print name
+ * @f: variable to hold string name
+ * @name: name of person
+ *
  * Return: void
  */
+void print_name(char *name, void (*f)(char *))
+{
+	/* set condition for when f/name points to/contains nothing */
+	if (name == NULL || f == NULL)
+		return; /* return nothing */
 
-void print_name(char *name, void (*f)(char *)) {
-    f(name);
+	/* call pointer to name string */
+	f(name);
 }
-
-void print_to_stdout(char *name) {
-    printf("Name: %s\n", name);
-}
-
-int main() {
-    char *name = "Kabi";
-    print_name(name, &print_to_stdout);
-    return 0;
-}
-
