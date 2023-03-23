f43fbdfe3ac78789bb914ae33002ac8e2938b674
234Kabi <234kabi@gmail.com>
2023-03-23 14:35:22 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,10 +1,10 @@
 #include "variadic_functions.h"
 
 /**
- * This function takes in a separator string and a variable number of strings,
- *  prints out all the strings separated by the given separator string.
- *  Define a pointer to a char (i.e. a string) and a counter variable.
- *  Declare a va_list variable which holds information about the variable arguments.
+ * print_strings - Prints strings passed to the function separed by separators
+ * @separator: A string that is used to separate strings
+ * @n: number of unnamed parameters
+ * Return: 0
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -13,40 +13,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	// Initialize the va_list variable with the variable argument list and the number of arguments.
 	va_start(ap, n);
-
-	// If the separator string is NULL, set it to an empty string.
 	if (separator == NULL)
-	{
-		separator = "";
-	}
-
-	// Loop through all the strings passed in as arguments.
+	separator = "";
 	for (i = 0; i < n; i++)
 	{
-		// Get the next string from the variable argument list.
-		str = va_arg(ap, char*);
-
-		// If the string is NULL, replace it with the string "(nil)".
-		if (str == NULL)
-		{
-			str = "(nil)";
-		}
-
-		// Print out the string.
-		printf("%s", str);
-
-		// If this is not the last string, print out the separator string.
-		if (i < n - 1)
-		{
-			printf("%s", separator);
-		}
+	str = va_arg(ap, char*);
+	if (str == NULL)
+	str = "(nil)";
+	printf("%s", str);
+	if (i < n - 1)
+	printf("%s", separator);
 	}
-
-	// Print a newline character at the end.
 	printf("\n");
-
-	// Clean up the va_list variable.
 	va_end(ap);
 }
