6244ef5eb10e2c51090ba24ac972c17ae9d1fa62
234Kabi <234kabi@gmail.com>
2023-06-10 01:47:02 +0200

    4-add.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,8 +4,7 @@
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
