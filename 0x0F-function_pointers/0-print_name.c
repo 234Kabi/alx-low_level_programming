f8bb76392d26e065c87212424fa09850578071e6
234Kabi <234kabi@gmail.com>
2023-06-21 23:19:22 +0200

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,15 +4,11 @@
  * print_name - function to print name
  * @f: variable to hold string name
  * @name: name of person
- *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	/* set condition for when f/name points to/contains nothing */
 	if (name == NULL || f == NULL)
-		return; /* return nothing */
-
-	/* call pointer to name string */
+		return 0;
 	f(name);
 }
